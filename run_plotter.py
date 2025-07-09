f4a42cdde0f8e6edd0d3c2d6fb6c74fb94fedd74
Subhodeep01 <ghoshdeep01@gmail.con>
2025-07-09 13:17:02 -0400

    ignored output tweets

--- run_plotter.py
+++ run_plotter.py
@@ -1,9 +1,9 @@
 import subprocess
 import time
 
-window_size = 100
-block_size = 10
-cardinality = 3
+window_size = 1000
+block_size = 25
+cardinality = 5
 metric_file = "metric_GENDER10005.csv"
 
 command = [f'python graph_plotter.py --window_size={window_size} --block_size={block_size} --cardinality={cardinality} --metric_file={metric_file}']
